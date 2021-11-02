d9b2d1cc698a69c737ecfc29188482eb5bc47902
Taylor Otwell <taylor@laravel.com>
2021-11-02 08:33:38 -0500

    Update AuthenticatesUsers.php

--- auth-backend/AuthenticatesUsers.php
+++ auth-backend/AuthenticatesUsers.php
@@ -45,6 +45,7 @@ trait AuthenticatesUsers
 
         if ($this->attemptLogin($request)) {
             $request->session()->put('auth.password_confirmed_at', time());
+
             return $this->sendLoginResponse($request);
         }
 
